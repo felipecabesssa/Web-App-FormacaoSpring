6a74d625fb2906dec6e3c0207cc61fccba672f23
felipecabesssa <felipesoares100@hotmail.com>
2021-11-24 22:32:13 -0300

    Criando o banco e Usando JPA para integrar

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,24 +15,15 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Plan B Team Foil Gustavo");
-		pedido.setUrlImagem("https://planbskateboards.com/wp-content/uploads/2021/03/plan-b-team-foil-gustavo-7-75-deck.jpg");
-		pedido.setUrlProduto("https://planbskateboards.com/product/skateboard/plan-b-team-foil-gustavo-deck/");
-		pedido.setDescricao("ADDITIONAL INFORMATION\r\n" + 
-				"Weight	1.2 kg\r\n" + 
-				"Sizes	\r\n" + 
-				"7.75\", 8.0\"\r\n" + 
-				"\r\n" + 
-				"Serie	\r\n" + 
-				"Team Foil\r\n" + 
-				"\r\n" + 
-				"PRO	\r\n" + 
-				"Felipe Gustavo");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
